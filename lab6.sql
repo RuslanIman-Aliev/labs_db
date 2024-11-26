3ec6494f73b4c8ef7fbdb9b9e297c97934405b31
RuslanIman-Aliev <122665831+RuslanIman-Aliev@users.noreply.github.com>
2024-11-26 17:42:14 +0100

    Lab 6

---
+++ lab6.sql
@@ -0,0 +1,6 @@
+﻿-- Отримати назву міста і назву регіону в якому знаходиться місто. У результат мають потрапити міста з населенням більше ніж 350 000.
+select cities.name, regions.name, population from cities, regions
+where regions.uuid = cities.region && population >=350000;
+-- За допомогою поєднань двох таблиць отримати міста яки знаходяться у регіоні з назвою Nord.
+select cities.name, regions.name  from cities, regions
+where regions.uuid = cities.region && regions.name="Nord";
