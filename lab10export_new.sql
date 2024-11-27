805dab3b80852b5f78807d664c059b563c42b46c
RuslanIman-Aliev <122665831+RuslanIman-Aliev@users.noreply.github.com>
2024-11-27 09:39:43 +0100

    Lab 10

---
+++ lab10export_new.sql
@@ -0,0 +1,28 @@
+﻿
+CREATE TABLE lines (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(100) NOT NULL,
+    Color VARCHAR(50) 
+);
+
+CREATE TABLE stations (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(100) NOT NULL,
+    Line_ID INT,
+    Coordinates VARCHAR(255),  
+    Previous_Station_ID INT NULL,   
+    Next_Station_ID INT NULL,       
+    FOREIGN KEY (Line_ID) REFERENCES sines(Id),
+    FOREIGN KEY (Previous_Station_ID) REFERENCES stations(Id),
+    FOREIGN KEY (Next_Station_ID) REFERENCES stations(Id)
+);
+
+CREATE TABLE line_transfers (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    From_Line_ID INT,   
+    To_Line_ID INT,     
+    Station_ID INT,     
+    FOREIGN KEY (From_Line_ID) REFERENCES lines(Id),
+    FOREIGN KEY (To_Line_ID) REFERENCES lines(Id),
+    FOREIGN KEY (Station_ID) REFERENCES stations(Id)
+);
